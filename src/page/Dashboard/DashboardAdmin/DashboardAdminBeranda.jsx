833c4d8caf5891e2d8f3d1b31f5068b755de95b9
Jojo297 <mafiffudin28@gmail.com>
2025-12-20 15:47:15 +0700

    add new card in dashboard admin

--- src/page/Dashboard/DashboardAdmin/DashboardAdminBeranda.jsx
+++ src/page/Dashboard/DashboardAdmin/DashboardAdminBeranda.jsx
@@ -19,17 +19,32 @@ import {
   ResponsiveContainer,
   LabelList,
 } from "recharts";
+import useGetProgramCampusChart from "@/hooks/hooksAdmin/useGetProgramCampusChart";
 
 export default function DashboardAdminBeranda() {
   const { dashboardData, isLoading, error, fetchDashboardData } =
     useGetDashboardData();
+  const {
+    programChartData,
+    isLoadingProgramChart,
+    errorProgramChart,
+    fetchProgramCampusChart,
+  } = useGetProgramCampusChart();
   const token = localStorage.getItem("userJwt");
   const decode = jwtDecode(token);
 
   const chartData = dashboardData?.program_per_campus ?? [];
 
   const displayDashboardData = dashboardData ?? [];
-  // console.log(displayDashboardData);
+  const displayProgramChartData = programChartData ?? [];
+  // console.log(displayProgramChartData);
+
+  // fetch program campus for chart
+  useEffect(() => {
+    if (token) {
+      fetchProgramCampusChart(token);
+    }
+  }, [token]);
 
   // fetch dashboard data (total campus, total program, total mentee and data chart)
   useEffect(() => {
@@ -60,6 +75,27 @@ export default function DashboardAdminBeranda() {
     return null;
   };
 
+  // card hover in chart mentee
+  const CustomTooltipMentee = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="bg-white p-3 rounded-lg shadow-lg border border-gray-200">
+          <p className="text-sm font-semibold text-gray-800">{label}</p>
+          <p className="text-xs text-gray-700">
+            Total Mentee:{" "}
+            <span className="font-bold text-base text-teal-600">
+              {payload[0].value}
+            </span>
+          </p>
+          <p className="text-xs text-gray-500 mt-1">
+            {payload[0].payload.campus_name}
+          </p>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="p-2 w-full">
       {/* HEADER */}
@@ -68,7 +104,7 @@ export default function DashboardAdminBeranda() {
         <h1 className="text-3xl font-bold text-[#003631]">{decode.username}</h1>
       </div>
 
-      {/* === STATS CARDS === */}
+      {/* === CARDS === */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 mb-6">
         {/* Card Total Program */}
         <div className="bg-white p-6 rounded-xl border border-gray-200 shadow-md flex items-center gap-5">
@@ -109,13 +145,13 @@ export default function DashboardAdminBeranda() {
           </div>
         </div>
       </div>
-      {/* === END STATS CARDS === */}
+      {/* === END CARDS === */}
 
       {/* === CHART SECTION === */}
       <section className="bg-white w-full rounded-xl p-6 text-gray-800 shadow-md mb-6 border border-gray-200">
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
           <h3 className="text-xl font-bold">
-            Jumlah Progam Berdasarkan Kampus
+            Jumlah Progam Berdasarkan Kampus{" "}
           </h3>
         </div>
 
@@ -170,6 +206,77 @@ export default function DashboardAdminBeranda() {
             // Jika tidak ada data program, tampilkan pesan
             <div className="flex flex-col gap-4 justify-center items-center h-full text-center p-4">
               <p className="text-gray-700 text-lg font-medium">
+                Belum ada Program yang terdaftar.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="text-center mt-4 text-xs text-gray-600">
+          Data Mentee (Per Program)
+        </div>
+      </section>
+      {/* === END CHART SECTION === */}
+
+      {/* === CHART PROGRAM SECTION === */}
+      <section className="bg-white w-full rounded-xl p-6 text-gray-800 shadow-md mb-6 border border-gray-200">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
+          <h3 className="text-xl font-bold">
+            Jumlah Pendaftaran Mentee per Program
+          </h3>
+        </div>
+
+        <div className="h-80 w-full">
+          {/* === START: Conditional Rendering === */}
+          {displayProgramChartData && displayProgramChartData.length > 0 ? (
+            // Jika ada data program, tampilkan BarChart
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart
+                data={displayProgramChartData}
+                margin={{ top: 20, right: 20, left: 0, bottom: 50 }}
+              >
+                <XAxis
+                  dataKey="program_name"
+                  stroke="#013D3A"
+                  tickLine={false}
+                  tick={{
+                    angle: -25,
+                    textAnchor: "middle",
+                    dy: 15,
+                  }}
+                  interval={0}
+                  height={50}
+                  style={{ fontSize: "10px" }}
+                />
+
+                <YAxis
+                  stroke="#013D3A"
+                  tickLine={false}
+                  axisLine={false}
+                  style={{ fontSize: "10px" }}
+                  allowDecimals={false}
+                  tickFormatter={(value) => `${value} Mentee`}
+                />
+
+                <Tooltip cursor={false} content={<CustomTooltipMentee />} />
+
+                <Bar
+                  dataKey="total_mentee"
+                  fill="#5CC6BA"
+                  radius={[4, 4, 0, 0]}
+                  minPointSize={5}
+                >
+                  <LabelList
+                    dataKey="total_mentee" // Pastikan ini sesuai dengan key di data Anda
+                    content={(props) => <text {...props} fill="#013D3A" />} // Mengubah warna label menjadi gelap
+                  />
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          ) : (
+            // Jika tidak ada data program, tampilkan pesan
+            <div className="flex flex-col gap-4 justify-center items-center h-full text-center p-4">
+              <p className="text-gray-700 text-lg font-medium">
                 Belum ada Kampus yang terdaftar.
               </p>
             </div>
@@ -180,6 +287,7 @@ export default function DashboardAdminBeranda() {
           Data Program (Per Kampus)
         </div>
       </section>
+      {/* === END CHART PROGRAM SECTION === */}
     </div>
   );
 }
